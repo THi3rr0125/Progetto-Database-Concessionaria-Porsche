470769bfdccec13e0c4895abd54906a29de1f45c
thierry grange <156133071+THi3rr0125@users.noreply.github.com>
2026-03-17 19:19:23 +0100

    Create database connection in connessione.php
    Establish a connection to the MySQL database with error handling.

---
+++ connessione.php
@@ -0,0 +1,12 @@
+<?php
+$host = "localhost";
+$user = "root";
+$password = "";
+$db = "concessionario_porsche"; // correggi nome db
+
+$conn = new mysqli($host, $user, $password, $db);
+
+if ($conn->connect_error) {
+    die("Connessione fallita: " . $conn->connect_error);
+}
+?>
