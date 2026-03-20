c1f15203e45617586361f56a6973c25782d24e99
thierry grange <156133071+THi3rr0125@users.noreply.github.com>
2026-03-20 12:05:08 +0100

    Change database connection from mysqli to PDO

--- connessione.php
+++ connessione.php
@@ -4,7 +4,7 @@ $user = "root";
 $password = "";
 $db = "concessionario_porsche"; // correggi nome db
 
-$conn = new mysqli($host, $user, $password, $db);
+$conn = new pdo($host, $user, $password, $db);
 
 if ($conn->connect_error) {
     die("Connessione fallita: " . $conn->connect_error);
